efae809d86bc8d27082416b9f672e2e1f6be5266
inugami <inugamiboss@gmail.com>
2022-03-26 10:14:45 -0700

    fix paragons typo

--- pages/index.js
+++ pages/index.js
@@ -81,7 +81,7 @@ export default function Home() {
               },
               {
                 logo: ParagonsLogo,
-                name: "Paragon",
+                name: "Paragons",
                 link: "https://paragonsdao.com/",
               },
               {
